e1b40190f07bd9a9449b8f7db872088d59027711
David Fridovich-Keil <david.fridovichkeil@gmail.com>
2018-07-15 16:57:17 -0700

    adding gitignore

--- ros/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/dfk/Documents/Developer/faSTPeople/ros/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/dfk/Documents/Developer/fastrack/ros/devel;/home/dfk/Documents/Developer/crazyflie_clean/ros/devel;/opt/ros/kinetic".split(';') if "/home/dfk/Documents/Developer/fastrack/ros/devel;/home/dfk/Documents/Developer/crazyflie_clean/ros/devel;/opt/ros/kinetic" != "" else []
